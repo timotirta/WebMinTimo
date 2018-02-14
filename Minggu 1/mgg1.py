8a08cfd95fe099d918854b80b0a812d8ac69da68
timotirta <timotius.tirtawan@gmail.com>
2018-02-14 20:44:51 +0700

    Commit Minggu1

---
+++ Minggu 1/mgg1.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import urllib.request
+webnya = input("Masukan Link : ")
+conn = urllib.request.urlopen(webnya)
+html = conn.read()
+soup = BeautifulSoup(html, 'html.parser')
+file = open("Tujuan.html","wb")
+file.write(html)
+file.close()
+ctr=1
+for link in soup.find_all('a'):
+	if link.get('href')[:4] == "http":
+		print(link.get('href'))
+		conn = urllib.request.urlopen(link.get('href'))
+		html = conn.read()
+		file = open("Hasil"+str(ctr)+".html","wb")
+		file.write(html)
+		file.close()
+		ctr+=1
