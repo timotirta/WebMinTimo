f4f37a7e6548fdf0ef7f83dfff7cc63d6d6621d2
timotirta <timotius.tirtawan@gmail.com>
2018-02-26 21:09:08 +0700

    commit algo softkom hampir benar

--- Softkom-1/Softkom-1/Form1.cs
+++ Softkom-1/Softkom-1/Form1.cs
@@ -18,10 +18,10 @@ namespace Softkom_1
             InitializeComponent();
         }
         List<Double[]> data = new List<Double[]>();
-        Double alpha = 0.005;
+        Double alpha = 0.05;
         String[] nama;
         Random rnd = new Random();
-        int iter = 0, iteration = 1000;
+        int iter = 0, iteration = 10000;
 
         int[] y = new int[1600];
         private void Form1_Load(object sender, EventArgs e)
@@ -64,17 +64,19 @@ namespace Softkom_1
             }*/
             while (iter < iteration)
             {
-                h = 0;
+                Console.WriteLine("Iterasi - " + iter);
+                Console.WriteLine("-------------------------------------------------");
                 for (int i = 0; i < data.Count; i++)
                 {
+                    h = 0;
                     for (int j = 0; j < data[i].Length; j++)
                     {
                         h += (data[i][j] * tetha[j]);
                     }
                     for (int j = 0; j < 11; j++)
                     {
-                        temptetha[j] = tetha[j] - alpha * (h - y[i]) * data[i][j];
-                        Console.WriteLine(temptetha[0]);
+                        temptetha[j] = tetha[j] - alpha * (((h - y[i]) * data[i][j]) / data.Count);
+                        //Console.WriteLine(temptetha[0]);
                     }
                     for (int j = 0; j < 11; j++)
                     {
@@ -82,18 +84,28 @@ namespace Softkom_1
                     }
                 }
                 //Console.WriteLine(temptetha[0]);
-                Console.WriteLine(tetha[0]);
+                //Console.WriteLine(tetha[0]);
                 //Console.WriteLine(h.ToString());
                 iter++;
+                //Console.WriteLine("H(x) = " + tetha[0] + " x0 " + tetha[1] + " x1 " + tetha[2] + " x2 " + tetha[3] + " x3 " + tetha[4] + " x4 " + tetha[5] + " x5 " + tetha[6] + " x6 " + tetha[7] + " x7 " + tetha[8] + " x8 " + tetha[9] + " x9 " + tetha[10] + " x10");
             }
             //MessageBox.Show("H(x) = " + tetha[0] + " x0 " + tetha[1] + " x1 " + tetha[2] + " x2 " + tetha[3] + " x3 " + tetha[4] + " x4 " + tetha[5] + " x5 " + tetha[6] + " x6 " + tetha[7] + " x7 " + tetha[8] + " x8 " + tetha[9] + " x9 " + tetha[10] + " x10");
-            Double[] xku = { 7.4, 0.7, 0, 1.9, 0.076, 11, 34, 0.9978, 3.51, 0.56, 9.4 };
+            Double[] xku1 = { 7.4, 0.59, 0.08, 4.4, 0.086, 6, 29, 0.9974, 3.38, 0.5, 9, 4 };
             Double hasil = 0;
-            for (int i = 0; i < xku.Length; i++)
+            for (int i = 0; i < 11; i++)
             {
-                hasil += tetha[i] * xku[i];
+                hasil += tetha[i] * xku1[i];
             }
-            MessageBox.Show("H = "+Convert.ToInt32(hasil));
+            if (hasil == xku1[11])
+            {
+                Console.WriteLine("Benar");
+            }
+            else
+            {
+                Console.WriteLine("H = " + hasil);
+                Console.WriteLine("Asli = " + xku1[11]);
+                Console.WriteLine("Salah! Beda = "+(hasil-xku1[11]));
+            }
         }
     }
 }
