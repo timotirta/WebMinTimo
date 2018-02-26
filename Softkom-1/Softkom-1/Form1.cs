3aa9a161558346d5fe645187f667bdc0f17757b4
timotirta <timotius.tirtawan@gmail.com>
2018-02-26 19:24:50 +0700

    commit laptop softkom

--- Softkom-1/Softkom-1/Form1.cs
+++ Softkom-1/Softkom-1/Form1.cs
@@ -18,13 +18,17 @@ namespace Softkom_1
             InitializeComponent();
         }
         List<Double[]> data = new List<Double[]>();
+        Double alpha = 0.005;
+        String[] nama;
+        Random rnd = new Random();
+        int iter = 0, iteration = 1000;
+
+        int[] y = new int[1600];
         private void Form1_Load(object sender, EventArgs e)
         {
             bool awal = true;
             StreamReader sr = new StreamReader("winequality-red.csv");
             int ctr = 0;
-            String[] nama;
-            int[] y = new int[1600];
             while (!sr.EndOfStream)
             {
                 if (awal)
@@ -32,7 +36,7 @@ namespace Softkom_1
                     nama = sr.ReadLine().Replace("\"", string.Empty).Split(';');
                     for (int i = 0; i < 1600; i++)
                     {
-                        data.Add(new Double[12]);
+                        data.Add(new Double[11]);
                     }
                     awal = false;
                 }
@@ -50,8 +54,46 @@ namespace Softkom_1
                     ctr++;
                 }
             }
-            Double[] tetha = new Double[11];
-            Double[] temptetha = new Double[11];
+            Double[] tetha = { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 };
+            Double[] temptetha = { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 };
+            Double h = new Double();
+            //Double[] temph = new Double[data.Count];
+            /*for (int i = 0; i < tetha.Length; i++)
+            {
+                tetha[i] = rnd.NextDouble();
+            }*/
+            while (iter < iteration)
+            {
+                h = 0;
+                for (int i = 0; i < data.Count; i++)
+                {
+                    for (int j = 0; j < data[i].Length; j++)
+                    {
+                        h += (data[i][j] * tetha[j]);
+                    }
+                    for (int j = 0; j < 11; j++)
+                    {
+                        temptetha[j] = tetha[j] - alpha * (h - y[i]) * data[i][j];
+                        Console.WriteLine(temptetha[0]);
+                    }
+                    for (int j = 0; j < 11; j++)
+                    {
+                        tetha[j] = temptetha[j];
+                    }
+                }
+                //Console.WriteLine(temptetha[0]);
+                Console.WriteLine(tetha[0]);
+                //Console.WriteLine(h.ToString());
+                iter++;
+            }
+            //MessageBox.Show("H(x) = " + tetha[0] + " x0 " + tetha[1] + " x1 " + tetha[2] + " x2 " + tetha[3] + " x3 " + tetha[4] + " x4 " + tetha[5] + " x5 " + tetha[6] + " x6 " + tetha[7] + " x7 " + tetha[8] + " x8 " + tetha[9] + " x9 " + tetha[10] + " x10");
+            Double[] xku = { 7.4, 0.7, 0, 1.9, 0.076, 11, 34, 0.9978, 3.51, 0.56, 9.4 };
+            Double hasil = 0;
+            for (int i = 0; i < xku.Length; i++)
+            {
+                hasil += tetha[i] * xku[i];
+            }
+            MessageBox.Show("H = "+Convert.ToInt32(hasil));
         }
     }
 }
