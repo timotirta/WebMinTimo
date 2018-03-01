37505c7713efd581a3b2ab27b5fa12149b1dc6b3
timotirta <timotius.tirtawan@gmail.com>
2018-03-01 19:50:11 +0700

    commit softkom

--- Softkom-1/Softkom-1/Form1.Designer.cs
+++ Softkom-1/Softkom-1/Form1.Designer.cs
@@ -28,21 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Iterasi";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(-4, -3);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Iterasi";
+            series1.Name = "Hnya";
+            series1.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(690, 548);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(682, 544);
+            this.Controls.Add(this.chart1);
             this.Name = "Form1";
-            this.Text = "m";
+            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- Softkom-1/Softkom-1/Form1.cs
+++ Softkom-1/Softkom-1/Form1.cs
@@ -18,13 +18,12 @@ namespace Softkom_1
             InitializeComponent();
         }
         List<Double[]> data = new List<Double[]>();
-        Double alpha = 0.05;
+        Double alpha = 0.6;
         String[] nama;
         Random rnd = new Random();
-        int iter = 0, iteration = 10000;
-
-        int[] y = new int[1600];
-        private void Form1_Load(object sender, EventArgs e)
+        int iter = 1, iteration = 1000;
+        Double[] y = new Double[1600];
+        public void loadData()
         {
             bool awal = true;
             StreamReader sr = new StreamReader("winequality-red.csv");
@@ -46,7 +45,7 @@ namespace Softkom_1
                     for (int i = 0; i < temp.Length; i++)
                     {
                         if (temp.Length - 1 == i)
-                            y[ctr] = Convert.ToInt32(temp[i].ToString());
+                            y[ctr] = Convert.ToDouble(temp[i].ToString());
                         else
                             data[ctr][i] = Convert.ToDouble(temp[i].ToString());
 
@@ -54,18 +53,37 @@ namespace Softkom_1
                     ctr++;
                 }
             }
-            Double[] tetha = { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 };
-            Double[] temptetha = { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 };
-            Double h = new Double();
-            //Double[] temph = new Double[data.Count];
-            /*for (int i = 0; i < tetha.Length; i++)
+            sr.Close();
+        }
+        public void saveTetha()
+        {
+            StreamWriter sw = new StreamWriter("savetet.txt");
+            sw.WriteLine(tetha[0] + ";" + tetha[1] + ";" + tetha[2] + ";" + tetha[3] + ";" + tetha[4] + ";" + tetha[5] + ";" + tetha[6] + ";" + tetha[7] + ";" + tetha[8] + ";" + tetha[9] + ";" + tetha[10]);
+            sw.Close();
+        }
+        public void loadTetha()
+        {
+            String[] temp;
+            StreamReader sr = new StreamReader("savetet.txt");
+            temp = sr.ReadLine().Split(';');
+            for (int i = 0; i < 11; i++)
             {
-                tetha[i] = rnd.NextDouble();
-            }*/
-            while (iter < iteration)
+                tetha[i] = Convert.ToDouble(temp[i]);
+            }
+            sr.Close();
+        }
+        Double[] tetha = { 0,0,0,0,0,0,0,0,0,0,0,0 };
+        Double[] temptetha = { 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1 };
+        Double h = 0.0;
+        Timer tku = new Timer();
+        List<Double> cfHasil = new List<Double>();
+        public void train()
+        {
+            if (iter < iteration)
             {
                 Console.WriteLine("Iterasi - " + iter);
                 Console.WriteLine("-------------------------------------------------");
+                Double hasilku = 0.0;
                 for (int i = 0; i < data.Count; i++)
                 {
                     h = 0;
@@ -75,7 +93,12 @@ namespace Softkom_1
                     }
                     for (int j = 0; j < 11; j++)
                     {
-                        temptetha[j] = tetha[j] - alpha * (((h - y[i]) * data[i][j]) / data.Count);
+                        temptetha[j] = tetha[j] - (alpha * (((h - y[i]) * data[i][j]) / (data.Count * 2)));
+                        //Console.WriteLine(temptetha[0]);
+                    }
+                    for (int j = 0; j < 11; j++)
+                    {
+                        hasilku += ((h - y[i]) * data[i][j]) / (1599 * 2);
                         //Console.WriteLine(temptetha[0]);
                     }
                     for (int j = 0; j < 11; j++)
@@ -83,14 +106,39 @@ namespace Softkom_1
                         tetha[j] = temptetha[j];
                     }
                 }
+                hasilku /= 11.0;
+                chart1.Series["Hnya"].Points.AddXY(iter + "", hasilku);
+                //if (hasilku <= 0) iter = iteration;
                 //Console.WriteLine(temptetha[0]);
                 //Console.WriteLine(tetha[0]);
                 //Console.WriteLine(h.ToString());
                 iter++;
+                saveTetha();
                 //Console.WriteLine("H(x) = " + tetha[0] + " x0 " + tetha[1] + " x1 " + tetha[2] + " x2 " + tetha[3] + " x3 " + tetha[4] + " x4 " + tetha[5] + " x5 " + tetha[6] + " x6 " + tetha[7] + " x7 " + tetha[8] + " x8 " + tetha[9] + " x9 " + tetha[10] + " x10");
             }
+            else
+            {
+                tku.Stop();
+                tampilkan();
+                saveTetha();
+            }
+        }
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            train();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //loadTetha();
+            loadData();
+            tku.Interval = 1;
+            tku.Tick += new EventHandler(timer1_Tick);
+            tku.Start();
+        }
+        public void tampilkan()
+        {
             //MessageBox.Show("H(x) = " + tetha[0] + " x0 " + tetha[1] + " x1 " + tetha[2] + " x2 " + tetha[3] + " x3 " + tetha[4] + " x4 " + tetha[5] + " x5 " + tetha[6] + " x6 " + tetha[7] + " x7 " + tetha[8] + " x8 " + tetha[9] + " x9 " + tetha[10] + " x10");
-            Double[] xku1 = { 7.4, 0.59, 0.08, 4.4, 0.086, 6, 29, 0.9974, 3.38, 0.5, 9, 4 };
+            Double[] xku1 = { 7.9, 0.32, 0.51, 1.8, 0.341, 17, 56, 0.9969, 3.04, 1.08, 9.2, 6 };
             Double hasil = 0;
             for (int i = 0; i < 11; i++)
             {
@@ -104,7 +152,7 @@ namespace Softkom_1
             {
                 Console.WriteLine("H = " + hasil);
                 Console.WriteLine("Asli = " + xku1[11]);
-                Console.WriteLine("Salah! Beda = "+(hasil-xku1[11]));
+                Console.WriteLine("Salah! Beda = " + (hasil - xku1[11]));
             }
         }
     }
